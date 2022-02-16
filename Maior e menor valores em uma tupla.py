2bc949f7471970e8db1747933112f1217b8af735
gabriellima03 <lima.gab@outlook.com>
2022-02-16 16:42:53 -0300

    estudos

---
+++ Maior e menor valores em uma tupla.py
@@ -0,0 +1,6 @@
+from random import randint
+num = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+print(f'Os números sorteados foram {num}')
+print(f'O maior valor sorteado foi {max(num)}')
+print(f'O menor valor sorteado foi {min(num)}')
+
