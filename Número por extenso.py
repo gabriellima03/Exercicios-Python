bc8db774ae04b6a2f16e6e3eb2ad568398498862
gabriellima03 <lima.gab@outlook.com>
2022-02-16 15:52:00 -0300

    estudos

--- Número por extenso.py
+++ Número por extenso.py
@@ -1,4 +1,6 @@
 num = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',' dezenove','vinte')
-opção = int(input('Digite um número: '))
+opção = int(input('Digite um número entre 0 e 20: '))
+while opção < 0 or opção > 20:
+    opção = int(input('Opção inválida. Digite um número entre 0 e 20: '))
 print(f'Você digitou o número {num[opção]}.')
 
